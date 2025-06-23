ec3454c5e4263463bede24d801b6ce57abaed17c
244728 <244728@PC107390>
2025-06-23 10:51:02 +0100

    Add project files.

---
+++ Game prototype 1/TestScreen.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Game_prototype_1
+{
+    partial class TestScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TCSelectionButton = new System.Windows.Forms.Button();
+            this.WCSelectionButton = new System.Windows.Forms.Button();
+            this.TLButton = new System.Windows.Forms.Button();
+            this.TTSelectionButton = new System.Windows.Forms.Button();
+            this.New_Game_button = new System.Windows.Forms.Button();
+            this.LoadGameButton = new System.Windows.Forms.Button();
+            this.OptionsButton = new System.Windows.Forms.Button();
+            this.ContinueButton = new System.Windows.Forms.Button();
+            this.Title_label = new System.Windows.Forms.Label();
+            this.SunPicBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.TClaveaPicBox = new System.Windows.Forms.PictureBox();
+            this.Warclave = new System.Windows.Forms.PictureBox();
+            this.PrototypeErrorMessage = new System.Windows.Forms.Label();
+            this.ListOfBuildAction = new System.Windows.Forms.CheckedListBox();
+            this.LabelTitaniumCount = new System.Windows.Forms.Label();
+            this.LabelTextTitanium = new System.Windows.Forms.Label();
+            this.ProductionTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.SunPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TClaveaPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Warclave)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TCSelectionButton
+            // 
+            this.TCSelectionButton.Location = new System.Drawing.Point(12, 12);
+            this.TCSelectionButton.Name = "TCSelectionButton";
+            this.TCSelectionButton.Size = new System.Drawing.Size(344, 322);
+            this.TCSelectionButton.TabIndex = 0;
+            this.TCSelectionButton.Text = "T-Clavean";
+            this.TCSelectionButton.UseVisualStyleBackColor = true;
+            // 
+            // WCSelectionButton
+            // 
+            this.WCSelectionButton.Location = new System.Drawing.Point(2308, 12);
+            this.WCSelectionButton.Name = "WCSelectionButton";
+            this.WCSelectionButton.Size = new System.Drawing.Size(344, 322);
+            this.WCSelectionButton.TabIndex = 1;
+            this.WCSelectionButton.Text = "WarCLavean";
+            this.WCSelectionButton.UseVisualStyleBackColor = true;
+            // 
+            // TLButton
+            // 
+            this.TLButton.Location = new System.Drawing.Point(2308, 962);
+            this.TLButton.Name = "TLButton";
+            this.TLButton.Size = new System.Drawing.Size(344, 322);
+            this.TLButton.TabIndex = 2;
+            this.TLButton.Text = "T_L";
+            this.TLButton.UseVisualStyleBackColor = true;
+            // 
+            // TTSelectionButton
+            // 
+            this.TTSelectionButton.Location = new System.Drawing.Point(12, 962);
+            this.TTSelectionButton.Name = "TTSelectionButton";
+            this.TTSelectionButton.Size = new System.Drawing.Size(344, 322);
+            this.TTSelectionButton.TabIndex = 3;
+            this.TTSelectionButton.Text = "True-T ";
+            this.TTSelectionButton.UseVisualStyleBackColor = true;
+            // 
+            // New_Game_button
+            // 
+            this.New_Game_button.AutoSize = true;
+            this.New_Game_button.Location = new System.Drawing.Point(1060, 523);
+            this.New_Game_button.Name = "New_Game_button";
+            this.New_Game_button.Size = new System.Drawing.Size(484, 71);
+            this.New_Game_button.TabIndex = 4;
+            this.New_Game_button.Text = "New Game ";
+            this.New_Game_button.UseVisualStyleBackColor = true;
+            this.New_Game_button.Click += new System.EventHandler(this.New_Game_button_Click);
+            // 
+            // LoadGameButton
+            // 
+            this.LoadGameButton.AutoSize = true;
+            this.LoadGameButton.Location = new System.Drawing.Point(1060, 638);
+            this.LoadGameButton.Name = "LoadGameButton";
+            this.LoadGameButton.Size = new System.Drawing.Size(484, 71);
+            this.LoadGameButton.TabIndex = 5;
+            this.LoadGameButton.Text = "Load Game";
+            this.LoadGameButton.UseVisualStyleBackColor = true;
+            // 
+            // OptionsButton
+            // 
+            this.OptionsButton.AutoSize = true;
+            this.OptionsButton.Location = new System.Drawing.Point(1060, 758);
+            this.OptionsButton.Name = "OptionsButton";
+            this.OptionsButton.Size = new System.Drawing.Size(484, 71);
+            this.OptionsButton.TabIndex = 6;
+            this.OptionsButton.Text = "Options";
+            this.OptionsButton.UseVisualStyleBackColor = true;
+            // 
+            // ContinueButton
+            // 
+            this.ContinueButton.Location = new System.Drawing.Point(1060, 405);
+            this.ContinueButton.Name = "ContinueButton";
+            this.ContinueButton.Size = new System.Drawing.Size(484, 71);
+            this.ContinueButton.TabIndex = 7;
+            this.ContinueButton.Text = "Continue";
+            this.ContinueButton.UseVisualStyleBackColor = true;
+            this.ContinueButton.Click += new System.EventHandler(this.ContinueButton_Click);
+            // 
+            // Title_label
+            // 
+            this.Title_label.AutoSize = true;
+            this.Title_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title_label.Location = new System.Drawing.Point(976, 157);
+            this.Title_label.Name = "Title_label";
+            this.Title_label.Size = new System.Drawing.Size(682, 59);
+            this.Title_label.TabIndex = 8;
+            this.Title_label.Text = "Welcome to the game user ";
+            this.Title_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Title_label.Click += new System.EventHandler(this.Title_label_Click);
+            // 
+            // SunPicBox
+            // 
+            this.SunPicBox.Location = new System.Drawing.Point(1886, 758);
+            this.SunPicBox.Name = "SunPicBox";
+            this.SunPicBox.Size = new System.Drawing.Size(434, 388);
+            this.SunPicBox.TabIndex = 9;
+            this.SunPicBox.TabStop = false;
+            this.SunPicBox.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(390, 748);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(434, 388);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // TClaveaPicBox
+            // 
+            this.TClaveaPicBox.Location = new System.Drawing.Point(390, 157);
+            this.TClaveaPicBox.Name = "TClaveaPicBox";
+            this.TClaveaPicBox.Size = new System.Drawing.Size(434, 388);
+            this.TClaveaPicBox.TabIndex = 12;
+            this.TClaveaPicBox.TabStop = false;
+            this.TClaveaPicBox.Visible = false;
+            // 
+            // Warclave
+            // 
+            this.Warclave.Location = new System.Drawing.Point(1869, 157);
+            this.Warclave.Name = "Warclave";
+            this.Warclave.Size = new System.Drawing.Size(434, 388);
+            this.Warclave.TabIndex = 13;
+            this.Warclave.TabStop = false;
+            this.Warclave.Visible = false;
+            // 
+            // PrototypeErrorMessage
+            // 
+            this.PrototypeErrorMessage.AutoSize = true;
+            this.PrototypeErrorMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.PrototypeErrorMessage.Location = new System.Drawing.Point(1040, 305);
+            this.PrototypeErrorMessage.Name = "PrototypeErrorMessage";
+            this.PrototypeErrorMessage.Size = new System.Drawing.Size(554, 29);
+            this.PrototypeErrorMessage.TabIndex = 14;
+            this.PrototypeErrorMessage.Text = "Please click the new game button to start the game";
+            this.PrototypeErrorMessage.Visible = false;
+            // 
+            // ListOfBuildAction
+            // 
+            this.ListOfBuildAction.FormattingEnabled = true;
+            this.ListOfBuildAction.Location = new System.Drawing.Point(1622, 235);
+            this.ListOfBuildAction.Name = "ListOfBuildAction";
+            this.ListOfBuildAction.Size = new System.Drawing.Size(216, 487);
+            this.ListOfBuildAction.TabIndex = 15;
+            this.ListOfBuildAction.Visible = false;
+            this.ListOfBuildAction.SelectedIndexChanged += new System.EventHandler(this.ListOfBuildAction_SelectedIndexChanged);
+            // 
+            // LabelTitaniumCount
+            // 
+            this.LabelTitaniumCount.AutoSize = true;
+            this.LabelTitaniumCount.Location = new System.Drawing.Point(1022, 14);
+            this.LabelTitaniumCount.Name = "LabelTitaniumCount";
+            this.LabelTitaniumCount.Size = new System.Drawing.Size(18, 20);
+            this.LabelTitaniumCount.TabIndex = 16;
+            this.LabelTitaniumCount.Text = "0";
+            this.LabelTitaniumCount.Visible = false;
+            // 
+            // LabelTextTitanium
+            // 
+            this.LabelTextTitanium.AutoSize = true;
+            this.LabelTextTitanium.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTextTitanium.Location = new System.Drawing.Point(902, 5);
+            this.LabelTextTitanium.Name = "LabelTextTitanium";
+            this.LabelTextTitanium.Size = new System.Drawing.Size(114, 29);
+            this.LabelTextTitanium.TabIndex = 17;
+            this.LabelTextTitanium.Text = "Titanium";
+            this.LabelTextTitanium.Visible = false;
+            // 
+            // ProductionTimer
+            // 
+            this.ProductionTimer.Interval = 4000;
+            this.ProductionTimer.Tick += new System.EventHandler(this.ProductionTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2391, 1186);
+            this.Controls.Add(this.LabelTextTitanium);
+            this.Controls.Add(this.LabelTitaniumCount);
+            this.Controls.Add(this.ListOfBuildAction);
+            this.Controls.Add(this.PrototypeErrorMessage);
+            this.Controls.Add(this.Warclave);
+            this.Controls.Add(this.TClaveaPicBox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.SunPicBox);
+            this.Controls.Add(this.Title_label);
+            this.Controls.Add(this.ContinueButton);
+            this.Controls.Add(this.OptionsButton);
+            this.Controls.Add(this.LoadGameButton);
+            this.Controls.Add(this.New_Game_button);
+            this.Controls.Add(this.TTSelectionButton);
+            this.Controls.Add(this.TLButton);
+            this.Controls.Add(this.WCSelectionButton);
+            this.Controls.Add(this.TCSelectionButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.SunPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TClaveaPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Warclave)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TCSelectionButton;
+        private System.Windows.Forms.Button WCSelectionButton;
+        private System.Windows.Forms.Button TLButton;
+        private System.Windows.Forms.Button TTSelectionButton;
+        private System.Windows.Forms.Button New_Game_button;
+        private System.Windows.Forms.Button LoadGameButton;
+        private System.Windows.Forms.Button OptionsButton;
+        private System.Windows.Forms.Button ContinueButton;
+        private System.Windows.Forms.Label Title_label;
+        private System.Windows.Forms.PictureBox SunPicBox;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox TClaveaPicBox;
+        private System.Windows.Forms.PictureBox Warclave;
+        private System.Windows.Forms.Label PrototypeErrorMessage;
+        private System.Windows.Forms.CheckedListBox ListOfBuildAction;
+        private System.Windows.Forms.Label LabelTitaniumCount;
+        private System.Windows.Forms.Label LabelTextTitanium;
+        private System.Windows.Forms.Timer ProductionTimer;
+    }
+}
+
---
+++ Game prototype 1/TestScreen.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace Game_prototype_1
+{
+    public partial class TestScreen : Form
+    {
+        class Resource// virtual class thingy
+
+        {
+            public int Amount;
+            private string Name;
+            public int Standard1;
+            public int Standard2;
+            public int Standard3;
+
+            public Resource(string name, int amount,int standard1, int standard2, int standard3)
+            {
+                Name = name;
+                Amount = amount;
+                Standard1 = standard1;
+                Standard2 = standard2;
+                Standard3 = standard3;
+            }
+            public int GetAmount()
+            {
+                return Amount;
+            }
+            public string GetName()
+            { 
+                return Name; 
+            }
+            public virtual int Standards( int stand = 1)
+            {
+                return 0;
+            }
+          
+        }
+        class Titanium : Resource
+        {
+            public Titanium() : base("titanium", 100,0,0,0) { }
+
+         
+            public override int Standards(int stand = 1)
+            {
+                switch (stand)
+                {
+                    case 1:
+                    { 
+                        return 2;
+                    }
+                       
+                    case 2:
+                    { 
+                        return 4;
+                    }
+
+                    case 3:
+                    {
+                        return 8;
+                    }
+                } 
+                return 0; 
+            }
+            class EnergyBricks : Resource
+            {
+                public EnergyBricks() : base("EnergyBricks", 200,0,0,0) { }
+
+                // will show the Standard of the producer 
+                public override int Standards(int stand = 1)
+                {
+                    switch (stand)
+                    {
+                        case 1:
+                            {
+                                return 1;
+                            }
+
+                        case 2:
+                            {
+                                return 3;
+                            }
+
+                        case 3:
+                            {
+                                return 6;
+                            }
+                    }
+                    return 0;
+                }
+            }
+        }
+        class ResourceManager
+        { public void FactoryStandListMaker(List<Button> TitaniumFacts)
+            {
+                foreach (Button B in TitaniumFacts)
+                {
+                    switch (B.Text)
+                    {
+                        case "Level1":
+                            {
+                                
+                            }
+                            break;
+                        case "Level2":
+                            {
+
+                            }
+                            break;
+
+                        case "Level3":
+                            {
+
+                            }
+                            break;
+
+                    }
+                }
+            } List<Resource> ResourcesList = new List<Resource>();
+            public int Resourcethingy(Resource producer)
+            {
+                ResourcesList.Add(producer);
+                foreach (Resource resource in ResourcesList)
+                    switch (resource.GetName())
+                    {
+                        case "Titanium":
+                            {
+                                FactoryStandListMaker(TileButtons)
+                            }
+                            break;
+
+
+                    }
+            }
+            
+
+                
+        }
+        // will handle all resoucre plusees
+
+       
+        List<Button> TileButtons = new List<Button>();
+       
+        List<string> ListedActions = new List<string> { "Factory", "Demolish", "Upgrade", };
+        List<bool> Factory = new List<bool>();
+        void GenOfTilePicBoxs(int WantedNum = 0)
+        {
+
+            int TileX = 400;
+            int TileY = 500;
+
+            for (int i = 0; i < WantedNum; i++)
+            {
+                TileButtons.Add(new Button());
+                TileButtons[i].Name = i.ToString();
+                TileButtons[i].Location = new System.Drawing.Point(TileX, TileY);
+                TileButtons[i].Size = new System.Drawing.Size(200, 200);
+                TileButtons[i].TabIndex = 1;
+                TileButtons[i].BackColor = Color.Yellow;
+                TileButtons[i].Text = "Standard1";
+                TileButtons[i].Enabled = true;
+                TileButtons[i].Click += new EventHandler(MyClickEvent);
+
+                this.Controls.Add(TileButtons[i]);
+                TileX += 200;
+                Factory.Add(false);
+
+            }
+
+        }
+
+        void newgame(bool x = false)
+
+        {
+
+            if (x)
+            {
+                TCSelectionButton.Show();
+                WCSelectionButton.Show();
+                TLButton.Show();
+                TTSelectionButton.Show();
+
+                Title_label.Hide();
+                ContinueButton.Hide();
+                New_Game_button.Hide();
+                LoadGameButton.Hide();
+                OptionsButton.Hide();
+
+
+            }
+            else
+            {
+                TCSelectionButton.Hide();
+                WCSelectionButton.Hide();
+                TLButton.Hide();
+                TTSelectionButton.Hide();
+
+                Title_label.Hide();
+                ContinueButton.Hide();
+                New_Game_button.Hide();
+                LoadGameButton.Hide();
+                OptionsButton.Hide();
+                ListOfBuildAction.Show();
+                LabelTitaniumCount.Show();
+                LabelTextTitanium.Show();
+                ListedBoxLoader();
+            }
+        }
+        public TestScreen()
+        {
+
+            InitializeComponent();
+
+        }
+
+        void ListedBoxLoader()
+        {
+
+
+
+            foreach (string I in ListedActions)
+            {
+                ListOfBuildAction.Items.Add(I);
+            }
+        }
+
+
+        private void Title_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ContinueButton_Click(object sender, EventArgs e)
+        {
+            PrototypeErrorMessage.Show();
+        }
+
+        private void New_Game_button_Click(object sender, EventArgs e)
+        {
+            PrototypeErrorMessage.Hide();
+            newgame();
+            GenOfTilePicBoxs(5);
+        }
+        void TileChecker(int IndexOfButton)
+        {
+            if (ListOfBuildAction.SelectedIndex > -1)
+            {
+                switch (ListedActions[ListOfBuildAction.SelectedIndex])
+                {
+                    case "Factory":
+                        TileButtons[IndexOfButton].BackColor = Color.Green;
+
+                        break;
+                    case "Demolish":
+                        TileButtons[IndexOfButton].BackColor = Color.Yellow;
+                        Factory[IndexOfButton] = false;
+                        break;
+                    case "Upgrade":
+                        {
+                            if (Convert.ToInt32(TileButtons[IndexOfButton].Text[5]) +1 < 4)
+                            { 
+                            TileButtons[IndexOfButton].Text = "Standard" + (Convert.ToInt32(TileButtons[IndexOfButton].Text[5]) + 1).ToString();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Titanium Factoires only go up to level three!");
+                            }
+                        }
+                        break;
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select what you would like to build For now the Titanium factory for now.....");
+            }
+
+        }
+      
+        int ButtonFinder(string NameOfButton)
+        {
+            for (int i = 0; i < TileButtons.Count; i++)
+            {
+                if (TileButtons[i].Name == NameOfButton)
+                {
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+        void FactoryChecker(int IndexOfButton)
+        {
+            if (TileButtons[IndexOfButton].BackColor == Color.Green)
+            {
+                Factory[IndexOfButton] = true;
+                FactoryProductAmount(TitaniumMaker());
+            }
+        }
+        int TitaniumMaker()
+        {   bool started = false;   
+            int i = 0;
+            foreach (bool x in Factory)
+            {
+                if (x)
+                {
+                    i++;
+                }
+            }
+            if (i > 0 && !started)
+            {
+                ProductionTimer.Start();
+                started = true; 
+            }
+            else if (i > 0)
+            {
+                return i; 
+            }
+            else
+            {
+                ProductionTimer.Stop(); 
+                started = false;
+            }
+            return i;
+        }
+      
+        void FactoryProductAmount(int AmountOfFacts)
+        {
+            int x = Convert.ToInt32(LabelTitaniumCount.Text);
+            x += AmountOfFacts;
+
+
+            LabelTitaniumCount.Text = x.ToString();
+        }
+        private void MyClickEvent(object sender, EventArgs e)
+        {
+
+            TileChecker(ButtonFinder(((Button)sender).Name));
+            FactoryChecker(ButtonFinder(((Button)sender).Name));
+
+        }
+
+        private void ListOfBuildAction_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void ProductionTimer_Tick(object sender, EventArgs e)
+        {
+            FactoryProductAmount(TitaniumMaker());
+        }
+    }
+}
