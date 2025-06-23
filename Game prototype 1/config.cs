712e992f7440eeab5b8fb2fb60da35dc7dc8c464
Taran Badesha <244728@esher.ac.uk>
2025-06-23 11:27:08 +0100

    New version 23/06/2025

---
+++ Game prototype 1/config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+
+namespace Game_prototype_1
+{
+    internal static class config
+    {
+        
+        public readonly static ImmutableArray<int> TitaniumUpgrades = ImmutableArray.Create(new[] { 0, 5, 50, 200 });
+    }
+}
